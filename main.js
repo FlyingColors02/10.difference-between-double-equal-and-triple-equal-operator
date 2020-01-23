9c3fab1e541abccb8c0bfbc1479cf1404c7855ce
FlyingColors02 <shraduborse97@gmail.com>
2020-01-24 01:27:55 +0530

    #shraddha borse: differentiating == and === operator

---
+++ main.js
@@ -0,0 +1,22 @@
+let numValue=10;
+let stringValue="10";
+
+if(numValue==stringValue)//true
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
+//here only value comparison is done 
+
+if(numValue===stringValue)//false
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
+//here value as well as data type is compared
